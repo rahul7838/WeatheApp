719b97dfd339b8acbc043c9a2deed45ac190a541
rahul7838 <rahulsinghal0308@gmail.com>
2019-09-02 14:41:27 +0530

    Added bottom sheet, main content, and activity main xml

--- app/src/main/java/com/example/weatherapp/ui/WeatherActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherActivity.kt
@@ -1,16 +1,50 @@
 package com.example.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.R
-import com.example.weatherapp.extension.add
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
-class WeatherActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity(), WeatherContract.View {
 
+    override fun showLoading() {
+
+    }
+
+    override fun hideLoading() {
+
+    }
+
+    private lateinit var bottomSheet: BottomSheetBehavior<View>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_container)
-        val weatherFragment = WeatherFragment()
-        add(weatherFragment, R.id.container, false)
+        setContentView(R.layout.activity_main)
+
+        bottomSheet = BottomSheetBehavior.from(layout_bottom_sheet)
+        bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheet.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(p0: View, p1: Float) {
+
+            }
+
+            override fun onStateChanged(p0: View, p1: Int) {
+
+            }
+
+        })
+//        val weatherFragment = WeatherFragment()
+//        add(weatherFragment, R.id.container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
     }
 }
