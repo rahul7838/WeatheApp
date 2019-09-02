2d5c85399bf6eb360b5e056789cf90e4ef676f10
rahul7838 <rahulsinghal0308@gmail.com>
2019-09-02 15:25:04 +0530

    Implemented font face of the text

--- app/src/main/java/com/example/weatherapp/ui/WeatherActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherActivity.kt
@@ -1,12 +1,14 @@
 package com.example.weatherapp.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
-import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class WeatherActivity : AppCompatActivity(), WeatherContract.View {
 
@@ -22,7 +24,7 @@ class WeatherActivity : AppCompatActivity(), WeatherContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        id_temp.typeface = getTypeFace("roboto_black.ttf")
         bottomSheet = BottomSheetBehavior.from(layout_bottom_sheet)
         bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
         bottomSheet.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
@@ -47,4 +49,8 @@ class WeatherActivity : AppCompatActivity(), WeatherContract.View {
         super.onResume()
         bottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
     }
+
+//    private fun getTypeFace(fileName: String): Typeface {
+//        return Typeface.createFromAsset(assets, "fonts/$fileName")
+//    }
 }
